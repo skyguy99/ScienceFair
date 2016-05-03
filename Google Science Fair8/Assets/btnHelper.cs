7a633bbf54680e5443dd00f8dd43b0f152bc5312
skyguy99 <coolsnt@gmail.com>
2016-05-02 21:25:51 -0700

    recent 2

---
+++ Google Science Fair8/Assets/btnHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseOver()
+	{
+		print ("BUTTON WORKS!");
+	}
+}
