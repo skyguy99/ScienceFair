f08d9d614924c6db19491762b144c8449ec5cd40
skyguy99 <coolsnt@gmail.com>
2016-05-08 20:44:08 -0700

    recent may

---
+++ Google Science Fair 16/Assets/Scripts/ViewSwap.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class ViewSwap : MonoBehaviour {
+	 
+	public static bool enableOkBtn = false;
+
+	public GameObject[] cardboardObjects;
+	public GameObject[] monoObjects;
+	public bool switched = false;
+	public Text advance;
+	//public Button okBtn;
+
+	//For trail renderer
+	public TrailRenderer trail;
+	public Material trailColor;
+	public Material trailInvisible;
+
+	// Turn on or off VR mode
+	void ActivateVRMode(bool goToVR) {
+		foreach (GameObject cardboardThing in cardboardObjects) {
+			cardboardThing.SetActive(goToVR);
+		}
+		foreach (GameObject monoThing in monoObjects) {
+			monoThing.SetActive(!goToVR);
+		}
+		Cardboard.SDK.VRModeEnabled = goToVR;
+
+		// Tell the game over screen to redisplay itself if necessary
+		//gameObject.GetComponent<GameController>().RefreshGameOver();
+	}
+
+	public void Switch() {
+		ActivateVRMode(false);
+		switched = true;
+		Debug.Log (switched+"No VR!");
+
+		trail.GetComponent<Renderer>().material = trailColor;
+	}
+	IEnumerator Switchback() 
+	{
+
+		//yield return new WaitForSeconds(3); 
+		advance.text = "Advance to next round in 3...";    
+		yield return new WaitForSeconds(1);  
+
+		advance.text = "Advance to next round in 2...";    
+		yield return new WaitForSeconds(1);  
+
+		advance.text = "Advance to next round in 1...";
+		yield return new WaitForSeconds(1);
+			ActivateVRMode(true);
+			switched = false;
+		trail.GetComponent<Renderer>().material = trailInvisible;
+
+		//okBtn.gameObject.SetActive (false);
+		//okBtn.GetComponentInChildren<CanvasRenderer>().SetAlpha(0);
+		//okBtn.GetComponentInChildren<Text>().color = Color.clear;
+
+
+			Debug.Log (switched+"VR!");
+		//round1.enabled = false;
+	}
+
+	void Update () {
+
+		//okBtn.enabled = true;
+		//okBtn.enabled = false;
+		//okBtn.image.sprite = null;
+
+		/*if (GameLogic.roundWasReset == true) {
+			advanceBtnHide ();
+			//GameLogic.roundWasReset = false;
+		}*/
+
+
+
+		if (switched == true) {
+			//round1.enabled = false;
+			StartCoroutine(Switchback ());
+		}
+
+		//Debug.Log ("updating");
+
+	}
+
+	void Start() {
+
+		//round1.enabled = true;
+		//round1.enabled = false;
+
+		trail.GetComponent<Renderer>().material = trailInvisible;
+		ActivateVRMode(true);
+	}
+}
