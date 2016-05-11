b54ccf067554df094262f9f06d3cbb526d3903ab
skyguy99 <coolsnt@gmail.com>
2016-05-10 22:00:51 -0700

    may 10

---
+++ Google Science Fair 21/Assets/Scripts/bottomUIScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class bottomUIScript : MonoBehaviour {
+
+	public Button giverUp;
+	public Button back;
+	public Sprite clicked1;
+	public Sprite clicked2;
+	public Sprite notClicked1;
+	public Sprite notClicked2;
+
+	public void backToMenu() {
+
+		SceneManager.LoadScene ("Menu2");
+		Debug.Log ("GO TO MENU!!");
+
+	}
+
+	public void changeImage1()
+	{
+		back.image.sprite = clicked1;
+	}
+	public void changeImageBack1()
+	{
+		back.image.sprite = notClicked1;
+	}
+	public void changeImageBack2()
+	{
+		giverUp.image.sprite = notClicked2;
+	}
+	public void changeImage2()
+	{
+		giverUp.image.sprite = clicked2;
+	}
+
+	void onMouseOver()
+	{
+		Debug.Log ("hovering!");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//back.GetComponent<SpriteRenderer> ().sprite = clicked1;
+
+	}
+}
---
+++ Google Science Fair 21/Assets/Scripts/loadGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadGame : MonoBehaviour {
+
+	//public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine (waitForGame ());
+
+	}
+	IEnumerator waitForGame()
+	{
+		yield return new WaitForSeconds (5);
+		//anim.Play ("New Animation");
+		SceneManager.LoadScene ("Menu2");
+	}
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("SPLASH SCREEN!!!");
+	
+	}
+}
